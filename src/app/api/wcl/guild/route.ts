30385c1a79579e9fab014b7a23d68a940e0727b8
Vollmer <kristianvollmer@gmail.com>
2025-07-16 21:25:20 +0200

    add `api/wcl/guild` endpoint

---
+++ src/app/api/wcl/guild/route.ts
@@ -0,0 +1,141 @@
+import { queryWcl } from "@/library/wcl/api";
+import { gql } from "graphql-request";
+
+export const query = gql`
+  query guild($id: Int, $name: String, $server: String, $region: String) {
+    guildData {
+      guild(id: $id, name: $name, serverSlug: $server, serverRegion: $region) {
+        description
+        id
+        name
+        server {
+          id
+          name
+          slug
+          region {
+            id
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
+
+export type GuildResponse = {
+  guildData: {
+    guild: {
+      description: string;
+      id: number;
+      name: string;
+      server: {
+        id: number;
+        name: string;
+        slug: string;
+        region: {
+          id: number;
+          slug: string;
+        };
+      };
+    };
+  };
+};
+
+export type Guild = {
+  id: number;
+  name: string;
+  server: string;
+  region: string;
+};
+
+type QueryVariables = Partial<{
+  id: number;
+  name: string;
+  server: string;
+  region: string;
+}>;
+
+function parseGuildResponse(data: GuildResponse): Guild {
+  return {
+    id: data.guildData.guild.id,
+    name: data.guildData.guild.name,
+    server: data.guildData.guild.server.name,
+    region: data.guildData.guild.server.region.slug,
+  };
+}
+
+function getVariablesFromBody(body: QueryVariables) {
+  if (!body) {
+    return new Response(JSON.stringify({ error: "Missing body" }), {
+      status: 400,
+    });
+  }
+  const { id, name, server, region } = body;
+
+  if (id) {
+    const parsedId = Number(id);
+    if (Number.isNaN(parsedId)) {
+      return new Response(
+        JSON.stringify({ error: "Param: 'id' must be a number" }),
+        {
+          status: 400,
+        }
+      );
+    }
+    return { id: parsedId };
+  }
+
+  if (!name) {
+    return new Response(
+      JSON.stringify({ error: "Param: 'name' is required" }),
+      {
+        status: 400,
+      }
+    );
+  }
+  if (!server) {
+    return new Response(
+      JSON.stringify({ error: "Param: 'server' is required" }),
+      {
+        status: 400,
+      }
+    );
+  }
+  if (!region) {
+    return new Response(
+      JSON.stringify({ error: "Param: 'region' is required" }),
+      {
+        status: 400,
+      }
+    );
+  }
+
+  return { name, server, region };
+}
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const maybeVariables = getVariablesFromBody(body);
+    if (maybeVariables instanceof Response) {
+      return maybeVariables;
+    }
+
+    const data = await queryWcl<GuildResponse, QueryVariables>(
+      query,
+      maybeVariables
+    );
+    const guild = parseGuildResponse(data);
+
+    return new Response(JSON.stringify(guild), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    const message =
+      err instanceof Error ? err.message : "Unknown error occurred";
+    return new Response(JSON.stringify({ error: message }), {
+      status: 500,
+    });
+  }
+}
